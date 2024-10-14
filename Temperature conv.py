b9ac7c5855195cccc66bc516ef0611535b3c8960
Omeh Brume <2411220@leedstrinity.ac.uk>
2024-10-14 05:04:14 -0700

    Temp conv

---
+++ Temperature conv.py
@@ -0,0 +1,15 @@
+# Step 1: Accept user input for temperature in degree Celsius.
+celsius = float(input("Enter temperature in Celsius: "))
+
+# Step 2: Apply the conversion formula.
+fahrenheit = (celsius * 9/5) + 32
+
+# Step 3: Displaying the converted temperature in Fahrenheit
+print(f"{celsius:.2f} degrees Celsius is equal to {fahrenheit:.2f} degrees Fahrenheit.")# Step 1: Accept user input for temperature in degree Celsius.
+celsius = float(input("Enter temperature in Celsius: "))
+
+# Step 2: Apply the conversion formula.
+fahrenheit = (celsius * 9/5) + 32
+
+# Step 3: Displaying the converted temperature in Fahrenheit
+print(f"{celsius:.2f} degrees Celsius is equal to {fahrenheit:.2f} degrees Fahrenheit.")

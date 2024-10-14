198882cae62f111554b00845cac0d8f0b5368b22
Omeh Brume <2411220@leedstrinity.ac.uk>
2024-10-14 04:32:41 -0700

    weekdays

---
+++ weekdays.py
@@ -0,0 +1,6 @@
+def main():
+    weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday","Saturday"]
+
+    weekdaysSize = len(weekdays)
+    for i in range(weekdaysSize):
+        print(weekdays[i][0])

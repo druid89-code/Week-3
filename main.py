883fb5b4bea10990e7eb93531ed64da120947da7
Omeh Brume <2411220@leedstrinity.ac.uk>
2024-10-14 03:32:42 -0700

    yes

---
+++ main.py
@@ -0,0 +1,5 @@
+def main():
+    for i in range(1, 13):
+        multiplyBy12 = i*12# Calculate this number for every iteration of the for loop
+        print(f"12 * {i} = {multiplyBy12}")
+        
